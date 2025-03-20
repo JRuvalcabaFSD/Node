88f66f3e33915164e26abd5951841af15f841efc
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-19 22:01:41 -0600

    Arow functions

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const { getUserById } = require("./js-fundations/03-callbacks")
 
 const id = 3
 
-getUserById(id, (err,user) => {
+const getUserById = (id, (err,user) => {
   if (err) throw new Error(err);
   console.log(user);
 })
