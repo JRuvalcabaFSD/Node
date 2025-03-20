37633a2a99447b47a40ded28579be48363359565
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-20 11:48:00 -0600

    Node and Typescript

---
+++ src/data/heroes.ts
@@ -0,0 +1,11 @@
+interface hero {
+  id: number;
+  name: string;
+  owner: string;
+}
+
+export const heroes: hero[] = [
+  { id: 1, name: 'Ironman', owner: 'Marvel' },
+  { id: 2, name: 'Spiderman', owner: 'Marvel' },
+  { id: 3, name: 'Batman', owner: 'DC' },
+];
