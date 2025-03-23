8436ee7a8599825741717d30925ef67ed1090e80
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-23 13:34:05 -0600

    Finich Calculator app sin cleen arquitecture

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import boxen from 'boxen';
+import path from 'path';
+import { mkdirSync, writeFileSync } from 'fs';
 import { yarg } from './config/plugins/yargs.plugin';
 
 (async () => {
@@ -5,5 +8,21 @@ import { yarg } from './config/plugins/yargs.plugin';
 })();
 
 async function main() {
-  console.log('Main ejecutado');
+  const { b: base, l: limit, s: show } = yarg;
+
+  let ouputMessage = '';
+  const headerMessage = boxen(`Tabla del ${base}`, { padding: 1, width: 180, borderColor: 'green', textAlignment: 'center' });
+  const ouputDir = path.join(__dirname, '../ouputs');
+
+  for (let i = 1; i <= limit; i++) {
+    ouputMessage += `${base} x ${i} = ${base * i}\n`;
+  }
+
+  ouputMessage = `${headerMessage}\n${ouputMessage}`;
+
+  mkdirSync(ouputDir, { recursive: true });
+  writeFileSync(`${ouputDir}/tabla-${base}.txt`, ouputMessage);
+
+  if (show) console.log(ouputMessage);
+  console.log('File created!');
 }
