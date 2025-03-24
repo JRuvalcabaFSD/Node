d8df7deede0a154420bfb6e3c2f0993a78032f34
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-23 18:49:19 -0600

    Implement test for app.ts

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,20 @@
+import { ServerApp } from './presentation/server.app';
+
 describe('App', () => {
-  test('should be true', () => {
-    expect(true).toBe(true);
+  test('should call Server.run width values', async () => {
+    const serverRunMock = jest.fn();
+
+    ServerApp.run = serverRunMock;
+    process.argv = ['node', 'app.ts', '-b', '10', '-l', '20', '-s', 'true', '-n', 'custom-name', '-d', 'custom-path'];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      fileDestination: 'custom-path',
+      fileName: 'custom-name',
+      limit: 20,
+      showTable: true,
+    });
   });
 });
