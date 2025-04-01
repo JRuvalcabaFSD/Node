32d9b097c08016459c7a1643da84d61bbd2b6084
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-03-31 19:37:06 -0600

    creatin Spaa

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import { join } from 'path';
 
 export class Server {
   private app = express();
   async start() {
     //* Middlewares
     //* Public folder
+
     this.app.use(express.static('public'));
+
+    this.app.get(/(.*)/, (req, res) => {
+      const indexPath = join(__dirname + '../../../public/index.html');
+      res.sendFile(indexPath);
+    });
 
     this.app.listen(3000, () => {
       console.log(`Server running on port ${3000}`);
