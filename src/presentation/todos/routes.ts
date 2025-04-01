868cd102965f6632dac4677a78eac214ab2c4385
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-01 00:12:31 -0600

    implement CRUD (read)

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -5,7 +5,9 @@ export class TodoRoutes {
   static get routes(): Router {
     const router = Router();
     const todoController = new TodosControler();
+
     router.get('/', todoController.getTodos);
+    router.get('/:id', todoController.getTodoById);
 
     return router;
   }
