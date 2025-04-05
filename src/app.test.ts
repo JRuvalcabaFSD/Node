822903092c0b4e07551825b9e244e060c809fa82
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-04 21:11:12 -0600

    Configure test environment and add configuration file for jest

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,13 @@
-describe('App', () => {
-  test('should to be true', () => {
-    expect(true).toBe(true);
+import { envs } from './config/envs';
+import { Server } from './presentation/server';
+
+jest.mock('./presentation/server');
+
+describe('Testing App', () => {
+  test('Should the server has been invoked with the expected methods', async () => {
+    await import('../src/app');
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({ port: envs.PORT, public_path: envs.PUBLIC_PATH, routes: expect.any(Function) });
+    expect(Server.prototype.start).toHaveBeenCalled();
   });
 });
