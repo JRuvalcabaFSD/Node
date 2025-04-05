12add23e001ac86276b79ce471cfe1776f73f7eb
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-04 20:14:04 -0600

    Refactor TodosController to use use-case classes for better separation of concerns and improved error handling

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,61 +1,58 @@
 import { Request, Response } from 'express';
-import { CreateDto, TodoRepository, UpdateDto } from '../../domain';
-import { prisma } from '../../data/postgres';
-import { Prisma } from '@prisma/client';
+import { CreateDto, CreateTodo, DeleteTodo, GetTodo, GetTodos, TodoRepository, UpdateDto, UpdateTodo } from '../../domain';
 
 export class TodosController {
   constructor(private readonly todoRepository: TodoRepository) {}
 
-  public getTodos = async (req: Request, res: Response) => {
-    const todos = await this.todoRepository.getAll();
-    console.log(todos);
-
-    res.json(todos);
-    return;
+  public getTodos = (req: Request, res: Response) => {
+    new GetTodos(this.todoRepository)
+      .execute()
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }));
   };
 
-  public getTodoById = async (req: Request, res: Response) => {
+  public getTodoById = (req: Request, res: Response) => {
     const id: number = +req.params.id;
-
-    try {
-      const todo = await this.todoRepository.findById(id);
-      res.json(todo);
-    } catch (error) {
-      res.status(400).json({ error });
-    }
+    new GetTodo(this.todoRepository)
+      .execute(id)
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }));
   };
 
-  public createTodo = async (req: Request, res: Response) => {
+  public createTodo = (req: Request, res: Response) => {
     const [error, todoData] = CreateDto.create(req.body);
-    if (error) res.status(error.httpCode).json({ error: error.message });
+    if (error) {
+      res.status(error.httpCode).json({ error: error.message });
+      return;
+    }
 
-    const todo = await this.todoRepository.create(todoData!);
-
-    res.json(todo);
+    new CreateTodo(this.todoRepository)
+      .execute(todoData!)
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }));
   };
 
-  public updateTodo = async (req: Request, res: Response) => {
+  public updateTodo = (req: Request, res: Response) => {
     const id = +req.params.id;
     const [err, dataTodo] = UpdateDto.create({ ...req.body, id });
 
-    if (err) res.status(err.httpCode).json(err.message);
-
-    try {
-      const todo = await this.todoRepository.updateById(dataTodo!);
-      res.json(todo);
-    } catch (error) {
-      res.status(400).json({ error });
+    if (err) {
+      res.status(err.httpCode).json(err.message);
+      return;
     }
+
+    new UpdateTodo(this.todoRepository)
+      .execute(dataTodo!)
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }));
   };
 
-  public deleteTodo = async (req: Request, res: Response) => {
+  public deleteTodo = (req: Request, res: Response) => {
     const id = +req.params.id;
 
-    try {
-      const todo = await this.todoRepository.deleteById(id);
-      res.json({ msg: `The element was eliminated with the ID ${id} correctly`, todo });
-    } catch (error) {
-      res.status(400).json({ error });
-    }
+    new DeleteTodo(this.todoRepository)
+      .execute(id)
+      .then((todos) => res.json(todos))
+      .catch((error) => res.status(400).json({ error }));
   };
 }
