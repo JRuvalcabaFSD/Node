834233410f90ec0f0f796cc226c5e72fab3af5f9
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-05 22:58:42 -0600

    Add authentication routes and controller for registration, login and mail validation

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+export class AuthController {
+  constructor() {}
+
+  registerUser = (req: Request, res: Response) => {
+    res.json('register user');
+  };
+  loginUser = (req: Request, res: Response) => {
+    res.json('login user');
+  };
+  validateEmail = (req: Request, res: Response) => {
+    res.json('validate email');
+  };
+}
