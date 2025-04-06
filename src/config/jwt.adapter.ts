aba653f364444d0e3baa4cb55182edd649109f19
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-06 11:38:29 -0600

    feat: add JWT_SEED to environment configuration and update JwtAdapter to use it for token generation

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,9 +1,12 @@
 import { sign, SignOptions } from 'jsonwebtoken';
+import { envs } from './envs';
+
+const jwtSeed = envs.JWT_SEED;
 
 export class JwtAdapter {
   static async generateToken(payload: any, duration: string = '2h') {
     return new Promise((res) => {
-      sign(payload, 'SEED', { expiresIn: duration } as SignOptions, (err, token) => {
+      sign(payload, jwtSeed, { expiresIn: duration } as SignOptions, (err, token) => {
         if (err) res(null);
         res(token);
       });
