3bbbb0a3e177fd726a6289bc253e4c2719943808
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-05 22:33:29 -0600

    Implementing basic node Server with TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hola Mundo!');
+import { envs } from './config/envs';
+import { AppRoutes } from './presentation/routes';
+import { AppServer } from './presentation/server';
+
+(async () => {
+  await main();
+})();
+
+function main() {
+  const server = new AppServer({
+    port: envs.PORT,
+    routes: AppRoutes.routes,
+  });
+
+  server.start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,50 @@
+import express, { Application, Request, Response, Router } from 'express';
+import { Server } from 'http';
+import { join } from 'path';
+
+interface Options {
+  port: number;
+  routes: Router;
+  publicPath?: string;
+}
+
+export class AppServer {
+  public readonly app: Application = express();
+  private serverListener?: Server;
+  private readonly port: number;
+  private readonly routes: Router;
+  private readonly publicPath: string;
+
+  constructor(options: Options) {
+    const { port, routes, publicPath = 'public' } = options;
+    this.port = port;
+    this.routes = routes;
+    this.publicPath = publicPath;
+  }
+
+  async start(): Promise<void> {
+    //*Middlewares
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    //*Public folder
+    this.app.use(express.static(this.publicPath));
+
+    //*Routes
+    this.app.use(this.routes);
+
+    //* SPA /^\/(?!api).*/  <== Únicamente si no empieza con la palabra api
+    this.app.get(/^\/(?!api).*/, (req: Request, res: Response) => {
+      const indexPath = join(__dirname, '../../../${ this.publicPath }/index.html');
+      res.sendFile(indexPath);
+    });
+
+    this.serverListener = this.app.listen(this.port, () => {
+      console.log(`Server running in port ${this.port}`);
+    });
+  }
+
+  public close(): void {
+    this.serverListener?.close();
+  }
+}
