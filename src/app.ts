7f8ed0d43b30ccb1370298b85e2916442d6aa924
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-05 23:10:13 -0600

    Add support for Mongodb connection and environment variable configuration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { MongoDatabase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { AppServer } from './presentation/server';
 
@@ -6,7 +7,8 @@ import { AppServer } from './presentation/server';
   await main();
 })();
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({ mongoUrl: envs.MONGO_URL, dbName: envs.MONGO_URL });
   const server = new AppServer({
     port: envs.PORT,
     routes: AppRoutes.routes,
