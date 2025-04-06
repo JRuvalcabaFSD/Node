46e38f80f043688283ed4469208f504b81a2a79c
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-05 19:10:35 -0600

    Refactor test cases for todos API, enhancing structure and adding data setup

--- src/presentation/todos/routes.test.ts
+++ src/presentation/todos/routes.test.ts
@@ -1,14 +1,37 @@
 import request from 'supertest';
 import { testServer } from '../../test-server';
+import { prisma } from '../../data/postgres';
 
 describe('todo route testing', () => {
+  const todo1 = { text: 'Hola Mundo 1' };
+  const todo2 = { text: 'Hola Mundo 2' };
+
   beforeAll(async () => {
     await testServer.start();
   });
   afterAll(() => {
     testServer.close();
   });
+  beforeEach(async () => {
+    await prisma.todo.deleteMany();
+  });
+
   test('should return todos api/todos', async () => {
-    await request(testServer.app).get('/api/todos').expect(200);
+    await prisma.todo.createMany({ data: [todo1, todo2] });
+    const { body } = await request(testServer.app).get('/api/todos').expect(200);
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].text).toBe(todo1.text);
+    expect(body[1].text).toBe(todo2.text);
+    expect(body[0].completedAt).toBeNull();
+  });
+
+  test('should return a todo api/todo/id', async () => {
+    const { id } = await prisma.todo.create({ data: todo1 });
+    const { body } = await request(testServer.app).get(`/api/todos/${id}`);
+
+    expect(body.text).toBe(todo1.text);
+    expect(body.completedAt).toBeNull();
   });
 });
