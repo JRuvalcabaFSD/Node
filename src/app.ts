81686a2769c80465b3fc5e35683fe50fc1898b14
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-10 01:06:23 -0600

    initial project

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hola Mundo!');
+import express, { Request, Response } from 'express';
+import { envs } from './config';
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.post('/api/github', (req: Request, res: Response) => {
+    res.json('Github endpoint');
+  });
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running on port ${envs.PORT}`);
+  });
+}
