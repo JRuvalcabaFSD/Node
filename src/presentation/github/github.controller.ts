0333e78adb3859f66369f22c699e98320d4f6dba
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-10 02:03:27 -0600

    Implement the github controller and handle webhook events

--- src/presentation/github/github.controller.ts
+++ src/presentation/github/github.controller.ts
@@ -1,12 +1,28 @@
 import { Request, Response } from 'express';
+import { GithubService } from '../services/github.service';
 
 export class GithubController {
-  constructor() {}
+  constructor(private readonly githubService = new GithubService()) {}
 
   webHookHandler = (req: Request, res: Response) => {
     const githubEvent = req.header('x-github-event') ?? 'unknown';
-    const signature = req.header('x-hub-signature-256') ?? 'unknown';
     const payload = req.body;
+    let message: string;
+
+    switch (githubEvent) {
+      case 'star':
+        message = this.githubService.onStart(payload);
+        break;
+      case 'issues':
+        message = this.githubService.onIssue(payload);
+        break;
+
+      default:
+        message = `Unknown event ${githubEvent}`;
+        break;
+    }
+
+    console.log({ message });
 
     res.status(202).send('Accepted');
   };
