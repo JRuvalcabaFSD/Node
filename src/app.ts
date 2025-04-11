c6ef7e91ec0a0cf6fa498c6510c6dfb63f3a97ca
Jesus Ruvalcaba <jruvalcabafsd@gmail.com>
2025-04-11 12:30:57 -0600

    Add Websocket Server and update dependencies in Package.json and yarn.lock

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log('Hola mundo!');
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+  console.log(ws);
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('Hola desde el servidor');
+  setInterval(() => {
+    ws.send('Hola de nuevo');
+  }, 2000);
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+console.log('Server running in port 3000');
